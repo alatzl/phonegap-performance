b68b9d8e38be2f6cde38014bbad52a7180fe5941
Alexandra Atzl <alexandra.atzl@effectiveui.com>
2013-11-19 14:12:35 -0500

    Init angular.

---
+++ app/scripts/controllers/main.js
@@ -0,0 +1,10 @@
+'use strict';
+
+angular.module('phonegapPerformanceApp')
+  .controller('MainCtrl', function ($scope) {
+    $scope.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
